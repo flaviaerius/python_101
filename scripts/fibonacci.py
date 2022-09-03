ac8abc893b87b3a5d4f37f257d93c6ea7ab40859
flaviaerius <flaviaerius@gmail.com>
2022-09-03 20:27:34 -0300

    fix: fixing range to count 0 and 1

--- scripts/fibonacci.py
+++ scripts/fibonacci.py
@@ -6,7 +6,7 @@ def fibonacci(num):
     print(0)
     print(1)
     fib = [0,1]
-    for i in range(num + 1):
+    for i in range(num - 1):
         sum = fib[len(fib) - 1] + fib[len(fib) - 2]
         fib.append(sum)
         print(fib[len(fib) - 1])
