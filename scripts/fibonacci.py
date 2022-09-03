ce961ecda8663f1a11f7e21767021ac756d5d002
flaviaerius <flaviaerius@gmail.com>
2022-09-03 20:21:00 -0300

    feat: fibonacci function

---
+++ scripts/fibonacci.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# Classic Fibonacci function
+
+def fibonacci(num):
+    print(0)
+    print(1)
+    fib = [0,1]
+    for i in range(num + 1):
+        sum = fib[len(fib) - 1] + fib[len(fib) - 2]
+        fib.append(sum)
+        print(fib[len(fib) - 1])
+
+fibonacci(10)
