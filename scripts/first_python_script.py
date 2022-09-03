514c614c8608e6667ed0c87c19899b16a0ada045
flaviaerius <flaviaerius@gmail.com>
2022-09-02 21:54:23 -0300

    feat: first script with list, tuple and dict

---
+++ scripts/first_python_script.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+# This script will be to practice while I learn Python from the course Python Core
+# of Sololearn
+
+# List 
+x = [1, 2, 3, 4, 5, 6]
+
+# Tuple - can't have values replaced
+y = ("one", "two", "three")
+
+# Dictionary - contains keys
+z = {"h": 1, "i": 2, "j": 3}
+
+# Lists can have their values substituted
+x[0] = 42
+
+# Just as dictionaries
+z["h"] = 78
+
+# We can reverse the list with
+x[::-1]
+
+# As well as the Tuple
+y[::-1]
+
+# But we can't do it with dictionaries - their index is only done with the keys
+
